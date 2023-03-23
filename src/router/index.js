92527e29f0654f4d6e3ad6dde4816179b745ff76
maumv7 <mauriciomolinamv01@gmail.com>
2023-03-23 15:42:27 -0500

    Se envia put

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,73 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+/* eslint-disable */
+import { createRouter, createWebHistory } from 'vue-router'
+import crearUsuario from '../components/crearUsuario.vue'
+import editarUsuario from '../components/editarUsuario.vue'
+import listarUsuario from '../components/listarUsuario.vue'
+import listarCliente from '../components/Cliente/listarCliente'
+import listarDepartamento from '../components/Departamento/listarDepartamento.vue'
+import listarEmpleado from '../components/Empleado/listarEmpleado.vue'
+import listarFactura from '../components/Factura/listarFactura.vue'
+import listarPuesto from '../components/Puesto/listarPuesto.vue'
+import listarRol from '../components/Rol/listarRol.vue'
+import Login from '../components/LoginCRUD.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'login',
+    component: Login
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
-    },
+    path: '/crear',
+    name: 'crear',
+    component: crearUsuario
   },
-];
+  {
+    path: '/listar',
+    name: 'listar',
+    component: listarUsuario
+  },
+  {
+    path: '/editar/:id',
+    name: 'editar',
+    component: editarUsuario
+  },
+  {
+    path: '/listarCliente',
+    name: 'listarCliente',
+    component: listarCliente
+  },
+  {
+    path: '/listarFactura',
+    name: 'listarFactura',
+    component: listarFactura
+  },
+  {
+    path: '/listarDepartamento',
+    name: 'listarDepartamento',
+    component: listarDepartamento
+  },
+  {
+    path: '/listarEmpleado',
+    name: 'listarEmpleado',
+    component: listarEmpleado
+  },
+  ,
+  {
+    path: '/listarPuesto',
+    name: 'listarPuesto',
+    component: listarPuesto
+  },
+  {
+    path: '/listarRol',
+    name: 'listarRol',
+    component: listarRol
+  }
+]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
-export default router;
+export default router
